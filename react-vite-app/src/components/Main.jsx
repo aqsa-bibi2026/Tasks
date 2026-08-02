a2a2f9ab8caa29b3353b4dd7447e94ffd869d3ae
aqsa-bibi2026 <allahdittaaqsa331@gmail.com>
2026-08-02 08:54:46 -0700

    submitt

---
+++ react-vite-app/src/components/Main.jsx
@@ -0,0 +1,10 @@
+function Main() {
+  return (
+    <main>
+      <h2>Welcome to React + Vite</h2>
+      <p>This is my first React project created using Vite.</p>
+    </main>
+  );
+}
+
+export default Main;
