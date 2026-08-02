f7d58ae94f09235f99f596925550c6bde842e1ba
aqsa-bibi2026 <allahdittaaqsa331@gmail.com>
2026-08-02 09:25:21 -0700

    submitt

---
+++ react-hooks-project/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Counter from "./components/Counter";
+import Users from "./components/Users";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Counter />
+      <Users />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
